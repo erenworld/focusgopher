6dd39e42e9afb13b92fdb1517eb107dd673b3549
erenworld <erenxturkoglu@gmail.com>
2025-09-16 10:42:22 +0200

    Added two command focus on/off

--- cli/commands.go
+++ cli/commands.go
@@ -8,3 +8,20 @@ type command struct {
 	Run  func() tea.Cmd
 }
 
+var commandFocusOn = command{
+	Name: "focus on",
+	Desc: "Enable focus window",
+	Run: func() tea.Cmd {
+		return nil
+	},
+}
+
+var commandFocusOff = command{
+	Name: "focus off",
+	Desc: "Disable focus window",
+	Run: func() tea.Cmd {
+		return nil
+	},
+}
+
+
