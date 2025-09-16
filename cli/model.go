1a2240e9f8463715c8f492683380544a8afb3511
erenworld <erenxturkoglu@gmail.com>
2025-09-16 16:19:14 +0200

    Added first CLI version

--- cli/model.go
+++ cli/model.go
@@ -6,12 +6,63 @@ import (
 )
 
 type model struct {
-	hostsManager			*hosts.Manager
-	commands				[]command
-	commandsListSelection	int
-	currentCommand			*command
-	initialised				bool
-	fatalErr				error
-	domains					[]string
+	hostsManager            *hosts.Manager
+	commands                []command
+	commandsListSelection   int
+	currentCommand          *command
+	initialised             bool
+	fatalErr                error
+	domains                 []string
 }
 
+func NewModel() model {
+	return model{
+		hostsManager: &hosts.Manager{},
+		commands:     []command{focusOn, focusOff, configureBlocklist},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return m.loadInitialConfig
+}
+
+type initResult struct {
+	err error
+}
+
+func (m model) loadInitialConfig() tea.Msg {
+	if err := m.hostsManager.Init(); err != nil {
+		return initResult{err: err}
+	}
+	return initResult{}
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case initResult:
+		m.initialised = true
+		if msg.err != nil {
+			m.fatalErr = msg.err
+			return m, tea.Quit
+		}
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "up", "k":
+			if m.commandsListSelection > 0 {
+				m.commandsListSelection--
+			}
+		case "down", "j":
+			if m.commandsListSelection < len(m.commands)-1 {
+				m.commandsListSelection++
+			}
+		case "enter", " ":
+			m.currentCommand = &m.commands[m.commandsListSelection]
+			return m, m.currentCommand.Run()
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package focusgopher
+package main
 
 import (
 	"fmt"
