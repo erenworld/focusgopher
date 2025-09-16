01651a1141f133a76e3c3804784466851eb7c82b
erenworld <erenxturkoglu@gmail.com>
2025-09-16 10:58:59 +0200

    Launch model in main

---
+++ main.go
@@ -0,0 +1,18 @@
+package focusgopher
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/erenworld/focusgopher/cli"
+)
+
+func main() {
+	model := cli.NewModel()
+	program := tea.NewProgram(model)
+	if _, err := program.Run(); err != nil {
+		fmt.Printf("Unable to launch focusgopher: %v", err)	
+		os.Exit(1)
+	}
+}
