2075d1d73c5bf797a52d386340ac5305042192b4
erenworld <erenxturkoglu@gmail.com>
2025-11-02 15:15:12 +0100

    Add on, off, configure blacklist commands to our cli

--- cli/commands.go
+++ cli/commands.go
@@ -7,3 +7,21 @@ type command struct {
 	Description string
 	RunFunc     func() tea.Cmd
 }
+
+var CommandFocusOn =  command{
+	CommandName: "focus on",
+	Description: "Start focus window.",
+	RunFunc: func() tea.Cmd { return nil },
+}
+
+var CommandFocusOff =  command{
+	CommandName: "focus off",
+	Description: "Stop focus window.",
+	RunFunc: func() tea.Cmd { return nil },
+}
+
+var ConfigureBlacklist =  command{
+	CommandName: "blacklist",
+	Description: "Configure blacklist.",
+	RunFunc: func() tea.Cmd { return nil },
+}
