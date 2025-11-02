4ae53f34ce10f6a40e5040738fe4d0c813abb021
erenworld <erenxturkoglu@gmail.com>
2025-11-02 15:57:13 +0100

    add all visual output depending on app state

--- cli/view.go
+++ cli/view.go
@@ -21,7 +21,41 @@ var statusStyle = lipgloss.NewStyle().
 
 var statusText = lipgloss.NewStyle().Inherit(statusBarStyle).Padding(0, 1)
 
-var listIconStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("99")).MarginRight(1)
+var listStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("99")).MarginRight(1)
 var listItemStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212")).MarginRight(1)
 
 var faint = lipgloss.NewStyle().Foreground(lipgloss.Color("250")).Faint(true)
+
+func (m model) View() string {
+	style := appHeaderStyle.Render("focusgopher") + faint.Render(" - block distractions") + "\n\n"
+
+	if !m.initialised {
+		style += "Loading current configuration...\n\n"
+	}
+
+	if m.fatalErr != nil {
+		bar := lipgloss.JoinHorizontal(lipgloss.Top, statusStyle.Render("ERROR"), statusText.Render(m.fatalErr.Error()))
+		
+		style += bar + "\n\n"
+	}
+
+	if m.initialised && m.fatalErr == nil {
+		l := list.New().Enumerator(func(items list.Items, i int) string {
+			if i == m.commandListSelection {
+				return "→"
+			}
+			return " "
+		}).
+		EnumeratorStyle(listStyle).
+		ItemStyle(listItemStyle)
+
+		for _, c := range m.commands {
+			l.Item(c.CommandName + faint.Render(" - " + c.Description))
+		}
+		style += l.String() + "\n\n"
+	}
+
+	style += "press q to quit.\n"
+
+	return style
+}
