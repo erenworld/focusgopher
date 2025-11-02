5298edbce6de79f59b036ea2cd1b03271bcae5f5
erenworld <erenxturkoglu@gmail.com>
2025-11-02 14:58:36 +0100

    Init new program with bubbletea package

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	
+	"github.com/erenworld/focusgopher/cli"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	model := cli.NewModel()
+	program := tea.NewProgram(model)
+	if _, err := program.Run(); err != nil {
+		fmt.Printf("Unable to launch focusgopher: %v", err)
+		os.Exit(1)
+	}
+}
