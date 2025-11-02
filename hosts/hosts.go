a45e50a1b074adc17863592c743f4ec188883dc0
erenworld <erenxturkoglu@gmail.com>
2025-11-02 16:04:15 +0100

    Init host with Manager struct that handle /etc/hosts

--- hosts/hosts.go
+++ hosts/hosts.go
@@ -1,1 +1,25 @@
 package hosts
+
+import "os"
+
+type Manager struct {
+	hostsFile *os.File
+}
+
+func (h *Manager) Init() error {
+	var err error
+	h.hostsFile, err = os.OpenFile("/etc/hosts", os.O_RDWR, 0600)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Manager) Close() error {
+	if h.hostsFile != nil {
+		return h.hostsFile.Close()
+	}
+
+	return nil
+}
